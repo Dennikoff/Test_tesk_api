39509e74c0f40edc0a087b85587afc618babbad5
Денис Харке <hda007@campus.mephi.ru>
2022-08-05 15:52:21 +0300

    add test to user log in

--- internal/app/apiserver/server_test.go
+++ internal/app/apiserver/server_test.go
@@ -3,6 +3,7 @@ package apiserver
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/Dennikoff/UserTagApi/internal/app/model"
 	"github.com/Dennikoff/UserTagApi/internal/app/store/teststore"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -59,6 +60,62 @@ func TestServer_HandleUsersCreate(t *testing.T) {
 			req, _ := http.NewRequest(http.MethodPost, "/signup", b)
 			s.ServeHTTP(rec, req)
 			assert.Equal(t, tc.statusCode, rec.Code)
+		})
+	}
+}
+
+func TestServer_HandleUserLogin(t *testing.T) {
+	user := &model.User{
+		Email:    "d.harke@yandex.ru",
+		Password: "12345678",
+		NickName: "kek",
+	}
+	s := newServer(teststore.New())
+	_ = s.store.User().Create(user)
+	testcases := []struct {
+		name       string
+		payload    interface{}
+		statusCode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]string{
+				"email":    "d.harke@yandex.ru",
+				"password": "12345678",
+			},
+			statusCode: http.StatusOK,
+		},
+		{
+			name: "invalidEmail",
+			payload: map[string]string{
+				"email":    "invalid@email",
+				"password": "12345678",
+			},
+			statusCode: http.StatusUnauthorized,
+		},
+		{
+			name: "invalidPassword",
+			payload: map[string]string{
+				"email":    "d.harke@yandex.ru",
+				"password": "123456",
+			},
+			statusCode: http.StatusUnauthorized,
+		},
+		{
+			name:       "invalidpayload",
+			payload:    3,
+			statusCode: http.StatusBadRequest,
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			b := &bytes.Buffer{}
+			json.NewEncoder(b).Encode(tc.payload)
+			req := httptest.NewRequest(http.MethodPost, "/login", b)
+			s.ServeHTTP(rec, req)
+			assert.Equal(t, tc.statusCode, rec.Code)
+
 		})
 	}
 }
